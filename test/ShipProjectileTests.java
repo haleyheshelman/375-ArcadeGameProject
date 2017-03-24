e022b482dd423da53467c57d129d8d21d09a226b
Vibha Alangar <alangavr@rose-hulman.edu>
2017-03-24 09:40:32 -0400

    working on projectile testing

---
+++ test/ShipProjectileTests.java
@@ -0,0 +1,66 @@
+import static org.junit.Assert.*;
+
+import java.awt.geom.Point2D;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ShipProjectileTests {
+	static Ship s; 
+	static ArcadeGame ag; 
+	
+	@Before
+	public void setUp() throws Exception {
+		Main.main(null); 
+		System.out.println("SETUP"); 
+		ag = new ArcadeGame(100,100); 
+		s = new Ship(ag, 10,16); 
+	}
+	
+	@After 
+	public void tearDown() throws Exception {
+		System.out.println("teardown");
+	}
+	
+	@Test
+	public void testConstructor () {
+		assertEquals(5,s.bombsRemaining);
+		assertEquals(1,s.getProjectileType());
+		assertEquals(0,ag.getProjectiles().size()); 
+		
+	}
+	
+	@Test 
+	public void testSetProjectileType (){
+		s.setProjectileType(2);
+		assertEquals(2,s.getProjectileType());
+		assertEquals(5, s.bombsRemaining);
+		assertEquals(0,ag.getProjectiles().size());
+		
+		s.setProjectileType(3);
+		assertEquals(3,s.getProjectileType());
+		assertEquals(5, s.bombsRemaining);
+		assertEquals(0,ag.getProjectiles().size());
+		
+		s.setProjectileType(4);
+		assertEquals(4,s.getProjectileType());
+		assertEquals(5, s.bombsRemaining);
+		assertEquals(0,ag.getProjectiles().size());
+		
+	}
+	
+	@Test 
+	public void testFireProjectileProjectileType1 () {
+		assertEquals(1, s.getProjectileType()); 
+		assertEquals(0, ag.getProjectiles());
+
+		
+		
+	}
+	
+	
+	
+
+}
