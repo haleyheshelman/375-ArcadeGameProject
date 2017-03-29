70a1e5ebae21b38fd0b595c3433ab8d63e624af8
Vibha Alangar <alangavr@rose-hulman.edu>
2017-03-29 12:09:25 -0400

    started Zombie monster class

---
+++ src/Zombie.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+
+
+public class Zombie extends Monster {
+
+	public Zombie(ArcadeGame game, double gridX, double gridY) {
+		super(game, gridX, gridY);
+		this.setColor(Color.MAGENTA);
+		
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
